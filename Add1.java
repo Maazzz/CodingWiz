f721662bbc6004d4e2a570064825eafc40492ab8
Maazzz <74023096+Maazzz@users.noreply.github.com>
2020-11-08 00:14:09 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,10 @@
+public class Add1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int s = 1;
+		for(int i=1;i<=5;i++) {
+			System.out.print(" "+i*s+"/"+i*i);
+			s = i*s;
+			}
+		}
+	}
